1bc0afc5e470de1595c41debe627a4eeaf97f0f3
Scott Coates <scoarescoare@gmail.com>
2021-12-23 00:14:04 -0600

    jsx ssr test

--- next-12/pages/index.js
+++ next-12/pages/index.js
@@ -64,6 +64,11 @@ export default function Home() {
           </span>
         </a>
       </footer>
+      <style jsx global>{`
+        * {
+          color: red !important;
+        }
+      `}</style>
     </div>
   )
 }
